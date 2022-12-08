30214d05c627f590ab2dcbf96218ef00ab274261
Leonardossev <leonardossev@gmail.com>
2022-12-08 00:34:05 -0300

    test: Checking operations

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+func main() {
+	d := CreateDeck()
+	fmt.Println("Creating a deck")
+	d.Print()
+
+	fmt.Println("Saving my deck")
+	SaveDeck("_mydeck.txt", d)
+
+	fmt.Println("Importing the deck from a file")
+	d2 := ImportDeck("_mydeck.txt")
+
+	fmt.Println("Shuffling the deck")
+	d2.Shuffle()
+	d2.Print()
+
+	fmt.Println("Dealing the cards")
+	h, rd := Deal(d2, 5)
+
+	fmt.Println("My hands")
+	h.Print()
+
+	fmt.Println("The rest of the deck")
+	rd.Print()
+}
